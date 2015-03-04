d977f6bcf9f179aca92e9fd71dd5ac6a0d6acda1
Melissa <trinhmw@gmail.com>
2015-03-04 15:12:35 -0600

    Fix some scrolling, added another textview and button for path activity

--- app/src/main/java/com/path/fusion/fusion/PathActivity.java
+++ app/src/main/java/com/path/fusion/fusion/PathActivity.java
@@ -19,9 +19,11 @@ import java.util.LinkedList;
 public class PathActivity extends ActionBarActivity {
     FileManager fileManager;
     Button mPathButton;
+    Button mAltPathButton;
     Spinner mCategorySpinner;
     Spinner mResultSpinner;
     TextView mTextView;
+    TextView mAltTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,43 +42,67 @@ public class PathActivity extends ActionBarActivity {
         View.OnClickListener pathOnClickListener = new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                if (validSpinner(fileManager.getUnique())) {
-                    PathAlgorithm pathAlgorithm = new PathAlgorithm();
-                    ArrayList<Vertex> ingredient = new ArrayList<>();
-                    ArrayList<Vertex> result = new ArrayList<>();
-                    String selection1 = mCategorySpinner.getSelectedItem().toString();
-                    String selection2 = mResultSpinner.getSelectedItem().toString();
-                    Pair<String, String> pair;
-                    pathAlgorithm.execute(fileManager.getUniqueHash().get(selection1));
-                    LinkedList<Vertex> path = pathAlgorithm.getPath(fileManager.getUniqueHash().get(selection2));
-                    LinkedList<Vertex> ingredientPath = null;
-                    mTextView.setText("");
-                    int i = 0;
-                    if (path == null) {
-                        mTextView.append("There is no existing path.");
-                    } else {
-                        for (Vertex vertex : path) {
-                            if (i != 0) {
-                                mTextView.append(" + " + fileManager.getMaterial(result.get(i - 1).getName(), vertex.getName()) + "=" + vertex.getName() + "\n");
-                            }
-                            if (i == 0) {
-                                mTextView.append(vertex.getName() + "\n");
-                            }
-
-                            result.add(vertex);
-                            mTextView.append(vertex.getName());
-                            Log.d("PathActivity", vertex.getName());
-                            i++;
-                        }
-                    }
-                }
+                mAltTextView.setText("");
+                findPath(mTextView, false);
             }
         };
         mPathButton.setOnClickListener(pathOnClickListener);
 
+        mAltTextView = (TextView) findViewById(R.id.altTextView);
 
-
+        mAltPathButton = (Button) findViewById(R.id.altPathButton);
+        View.OnClickListener altPathOnClickListener = new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                findPath(mAltTextView, true);
+            }
+        };
+        mAltPathButton.setOnClickListener(altPathOnClickListener);
     }
+
+    /**
+     * findPath - Find the path using the PathAlgorithm and sets the path in the specified textview.
+     * It may or may not append to the text view depending on the boolean.
+     * @param textView
+     * @param append
+     */
+    public void findPath(TextView textView, boolean append){
+        if (validSpinner(fileManager.getUnique())) {
+            PathAlgorithm pathAlgorithm = new PathAlgorithm();
+            ArrayList<Vertex> ingredient = new ArrayList<>();
+            ArrayList<Vertex> result = new ArrayList<>();
+            String selection1 = mCategorySpinner.getSelectedItem().toString();
+            String selection2 = mResultSpinner.getSelectedItem().toString();
+            Pair<String, String> pair;
+            pathAlgorithm.execute(fileManager.getUniqueHash().get(selection1));
+            LinkedList<Vertex> path = pathAlgorithm.getPath(fileManager.getUniqueHash().get(selection2));
+            LinkedList<Vertex> ingredientPath = null;
+            if(append == false) {
+                textView.setText("");
+            }
+            int i = 0;
+            if (path == null) {
+                textView.append("There is no existing path.");
+            } else {
+                for (Vertex vertex : path) {
+                    if (i != 0) {
+                        textView.append(" + " + fileManager.getMaterial(result.get(i - 1).getName(), vertex.getName()) + "=" + vertex.getName() + "\n");
+                    }
+                    if (i == 0) {
+                        textView.append(vertex.getName() + "\n");
+                    }
+
+                    result.add(vertex);
+                    textView.append(vertex.getName());
+                    Log.d("PathActivity", vertex.getName());
+                    i++;
+                }
+            }
+            textView.append("\n");
+        }
+    }
+
+
 
     /**
      * validSpinner - Checks content of the Arraylist for the adapter to make sure it's not null or empty
